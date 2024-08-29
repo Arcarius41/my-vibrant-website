051a53d0792632b08388005ed6b12deeb67f1fd1
Scott B Bowman <145110529+Arcarius41@users.noreply.github.com>
2024-08-29 00:33:45 +0000

    added a local image to about page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
-// src/About.js
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./About.css";
+import Portrait from './assets/images/B&WPortrait.jpg'; // Import the image
 
 function About() {
   return (
@@ -24,14 +24,14 @@ function About() {
           <p>
             When I'm not coding, you can find me playing hide-n-seek with my 5
             year old daughter, building pc's, playing fantasy crpg's on my
-            computer, serving at my church, hiking or working out . I believe in
+            computer, serving at my church, hiking or working out. I believe in
             continuous learning and always strive to stay updated with the
             latest industry trends.
           </p>
         </div>
         <div className="col-md-6 text-center">
           <img
-            src="https://i.ibb.co/cb3cQ64/B-WPortrait.jpg"
+            src={Portrait}  // Use the imported image
             alt="Portrait of Scott Bowman"
             className="img-fluid rounded-circle mb-4"
           />
@@ -61,31 +61,6 @@ function About() {
           <div className="d-flex justify-content-center flex-wrap">
             <span className="badge bg-primary m-2">Java</span>
             <span className="badge bg-primary m-2">C++</span>
-            {/* <img
-              src="https://via.placeholder.com/150"
-              alt="Gallery 1"
-              className="img-thumbnail m-2"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Gallery 2"
-              className="img-thumbnail m-2"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Gallery 3"
-              className="img-thumbnail m-2"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Gallery 4"
-              className="img-thumbnail m-2"
-            />
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Gallery 5"
-              className="img-thumbnail m-2"
-            /> */}
           </div>
           <div className="row mt-5">
             <div className="col-12">
@@ -96,19 +71,6 @@ function About() {
               </div>
             </div>
           </div>
-          {/* <div className="row mt-5">
-            <div className="col-12">
-              <h3 className="text-center">Soft Skills</h3>
-              <div className="d-flex justify-content-center flex-wrap">
-              <a
-                              href="/serenity-scribe"
-                              className="btn btn-warning"
-                            >
-                              Learn More
-                            </a>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
